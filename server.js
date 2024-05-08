d0f38e686c880f337bae0b7d2831bed0d2236f05
Łukasz Olbrycht <lukasz.olbrycht@gmail.com>
2024-05-08 10:53:12 +0200

    Add basic structure of server

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const db = require('./db/db');
+const socket = require('socket.io');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '/client')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/index.html'));
+});
+
+
+const server = app.listen( 8000, () => {
+    console.log('Server is running on port: 8000');
+});
+
+const io = socket(server);
+
